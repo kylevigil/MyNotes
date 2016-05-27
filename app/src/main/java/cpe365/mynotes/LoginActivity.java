83229e7fb9bee85186385bd89d10f4fe368cd3a6
Kyle <kyle.vigil831@gmail.com>
2016-05-27 00:56:48 -0700

    Added main page and create note page as well as got login page to render

--- app/src/main/java/cpe365/mynotes/LoginActivity.java
+++ app/src/main/java/cpe365/mynotes/LoginActivity.java
@@ -3,6 +3,7 @@ package cpe365.mynotes;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -49,7 +50,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "foo@example.com:hello", "bar@example.com:world", "a@a:12345"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -324,7 +325,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
 
             // TODO: register the new account here.
-            return true;
+            return false;
         }
 
         @Override
@@ -334,6 +335,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 finish();
+                Intent notes = new Intent(LoginActivity.this, NotesList.class);
+                LoginActivity.this.startActivity(notes);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
---
+++ app/src/main/java/cpe365/mynotes/NotesList.java
@@ -0,0 +1,29 @@
+package cpe365.mynotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class NotesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent note = new Intent(NotesList.this, CreateNote.class);
+                NotesList.this.startActivity(note);
+            }
+        });
+    }
+}
