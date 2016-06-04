9cace15aeea805566f8966f7d819b6973422feec
Kyle <kyle.vigil831@gmail.com>
2016-06-03 22:37:08 -0700

    Login page cleaned up, implemented background notes retrieval in notes list

--- app/src/main/java/cpe365/mynotes/NotesList.java
+++ app/src/main/java/cpe365/mynotes/NotesList.java
@@ -1,11 +1,27 @@
 package cpe365.mynotes;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class NotesList extends AppCompatActivity {
 
@@ -16,14 +32,88 @@ public class NotesList extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent notes = new Intent(NotesList.this, CreateNote.class);
-                NotesList.this.startActivity(notes);
-            }
-        });
+        Bundle bundle = getIntent().getExtras();
+        RetrieveNotesList getNotes = new RetrieveNotesList(bundle.getString("username"),
+                bundle.getString("passHash"));
+
+        getNotes.execute((Void) null);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.createNote);
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent notes = new Intent(NotesList.this, CreateNote.class);
+                    NotesList.this.startActivity(notes);
+                }
+            });
+        }
     }
 
+
+    public class RetrieveNotesList extends AsyncTask<Void, Void, Boolean> {
+        public final String mUsername;
+        public final String mHash;
+        public JSONObject notesList;
+
+        public RetrieveNotesList(String username, String passHash) {
+            mUsername = username;
+            mHash = passHash;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            HttpURLConnection urlConnection = null;
+            try {
+                URL url = new URL("http://52.38.152.182:8888");
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                Map<String,Object> postParams = new LinkedHashMap<>();
+                postParams.put("method","getNotes");
+                postParams.put("username", mUsername);
+                postParams.put("passHash", mHash);
+
+                StringBuilder postData = new StringBuilder();
+
+                for (Map.Entry<String,Object> param : postParams.entrySet()) {
+                    if (postData.length() != 0) postData.append('&');
+                    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                    postData.append('=');
+                    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+                }
+                byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setDoOutput(true);
+                conn.getOutputStream().write(postDataBytes);
+
+                Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+                String json = "";
+                for (int c; (c = in.read()) >= 0;)
+                    json += (char)c;
+
+                notesList = new JSONObject(json);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            if (urlConnection != null) urlConnection.disconnect();
+
+            return true;
+        }
+
+        protected void onPostExecute(final Boolean success) {
+            Context context = getApplicationContext();
+            CharSequence text = notesList.toString();
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
 }
