d83c382aa2d539d84c466a934febbf6744665c46
jakep11 <j.pickett67@yahoo.com>
2016-06-06 17:13:40 -0700

    #trying to fix this shit

--- app/src/main/java/cpe365/mynotes/CreateNote.java
+++ app/src/main/java/cpe365/mynotes/CreateNote.java
@@ -1,10 +1,12 @@
 package cpe365.mynotes;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,7 +17,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -24,6 +25,10 @@ import java.util.regex.Pattern;
 public class CreateNote extends AppCompatActivity {
     private EditText mTitle;
     private EditText mNote;
+    private boolean modify = false;
+    private String noteId = "";
+
+    private SaveNoteTask mSaveNote = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +40,37 @@ public class CreateNote extends AppCompatActivity {
         mTitle = (EditText) findViewById(R.id.titleInput);
         mNote = (EditText) findViewById(R.id.contentInput);
 
+
+        if (getIntent().hasExtra("existing")) {
+            modify = true;
+            noteId = getIntent().getStringExtra("noteId");
+            mTitle.append(getIntent().getStringExtra("title"));
+            mNote.append(getIntent().getStringExtra("noteText"));
+        }
+
         FloatingActionButton save = (FloatingActionButton) findViewById(R.id.save);
         if (save != null) {
             save.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    SaveNote saver = new SaveNote(mTitle.getText().toString(), mNote.getText().toString());
-                    saver.execute((Void) null);
-                    Intent notes = new Intent(CreateNote.this, NotesList.class);
-                    CreateNote.this.startActivity(notes);
+
+                    if (mTitle.getText().toString().length() == 0) {
+                        mTitle.setError(getString(R.string.no_title));
+                        mTitle.requestFocus();
+                    } else {
+                        if (mSaveNote == null) {
+                            mSaveNote = new SaveNoteTask(mTitle.getText().toString(), mNote.getText().toString(), modify, noteId);
+                            mSaveNote.execute((Void) null);
+                        }
+                        finish();
+                        Intent notes = new Intent(CreateNote.this, NotesList.class);
+                        CreateNote.this.startActivity(notes);
+                    }
                 }
             });
         }
     }
+
 
     public static boolean isGoodTag(String noteText) {
         String[] words = noteText.split(" ");
@@ -72,13 +95,39 @@ public class CreateNote extends AppCompatActivity {
         return true;
     }
 
-    public class SaveNote extends AsyncTask<Void, Void, Boolean> {
+
+    public void onBackPressed() {
+        AlertDialog.Builder addUserDialog = new AlertDialog.Builder(CreateNote.this);
+        addUserDialog.setMessage(R.string.confirm_return)
+                .setPositiveButton("Discard", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (!modify) {
+                            finish();
+                            CreateNote.this.startActivity(new Intent(CreateNote.this, NotesList.class));
+                        } else {
+                            finish();
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        addUserDialog.show();
+    }
+
+    public class SaveNoteTask extends AsyncTask<Void, Void, Boolean> {
         private final String mTitle;
         private final String mNote;
+        private final boolean mModify;
+        private final String mNoteId;
 
-        public SaveNote(String title, String note) {
+        public SaveNoteTask(String title, String note, boolean modify, String noteId) {
             mTitle = title;
             mNote = note;
+            mModify = modify;
+            mNoteId = noteId;
         }
 
         @Override
@@ -92,7 +141,12 @@ public class CreateNote extends AppCompatActivity {
 
                 String username = PreferenceManager.getDefaultSharedPreferences(CreateNote.this).getString("username", "xxxx");
                 String passHash = PreferenceManager.getDefaultSharedPreferences(CreateNote.this).getString("passHash", "xxxx");
-                postParams.put("method","addNote");
+                if (mModify) {
+                    postParams.put("method", "modifyNote");
+                    postParams.put("id", mNoteId);
+                } else {
+                    postParams.put("method", "addNote");
+                }
                 postParams.put("username", username);
                 postParams.put("passHash", passHash);
                 postParams.put("title", mTitle);
@@ -125,7 +179,9 @@ public class CreateNote extends AppCompatActivity {
             return false;
         }
 
+        @Override
         protected void onPostExecute(final Boolean success) {
+            mSaveNote = null;
             if (!success) {
                 Toast toast = Toast.makeText(CreateNote.this, R.string.fail, Toast.LENGTH_LONG);
                 toast.show();
@@ -134,6 +190,10 @@ public class CreateNote extends AppCompatActivity {
                 toast.show();
             }
         }
-    }
 
+        @Override
+        protected void onCancelled() {
+            mSaveNote = null;
+        }
+    }
 }
